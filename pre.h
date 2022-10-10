6c6832fd917bffa7635012deaffc4b32841f667f
Arthur <92685845+Artur420Art@users.noreply.github.com>
2022-10-10 13:04:05 +0400

    Create pre.h

---
+++ pre.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+class Pre {
+public:
+    Pre();
+    void pre_process(const std::string&);
+private:
+    typedef void(Pre::*func)(const std::string&);
+    bool flag;
+private:
+    std::fstream fin;
+    std::fstream fout;
+    std::map <std::string, func> inst;
+    std::map <std::string, std::string> defines;
+    std::vector<std::string> split(const std::string& str, std::string spliter);
+private:
+    void include(const std::string& str);
+    void define(const std::string& str);
+    void definition(std::string& str);
+    void undef(const std::string& str);
+    void ifndef(const std::string& str);
+    void ifdef(const std::string& str);
+    void endif(const std::string& str);
+    void make();
+};
